c0343468720b20ee78475200ee85ed3b08072fc7
Atakanhrn <atakanhrn@gmail.com>
2018-12-27 23:53:58 +0300

    Initial commit.

--- parser.py
+++ parser.py
@@ -1,1 +1,23 @@
 from bs4 import BeautifulSoup
+from request import Request
+
+
+class Parser:
+    def __init__(self):
+        self.request = Request()
+
+    def get_html(self, url):
+        response = self.request.simple_get(url)
+        if response is not None:
+            return BeautifulSoup(response, "html.parser")
+        else:
+            print("Response error")
+
+    def parse_html(self, url):
+        html = self.get_html(url)
+        university_divs = (html.find_all(class_ = "panel panel-primary", href = True))
+        for div in university_divs:
+            a = div.select("a")
+            print(a)
+        #print(html)
+
--- request.py
+++ request.py
@@ -2,22 +2,23 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 
+class Request:
+    def simple_get(self, url):
+        try:
+            with closing(get(url, stream=True)) as resp:
+                if self.is_good_response(resp):
+                    return resp.content
+                else:
+                    return None
 
-def simple_get(url):
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        print('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None
+        except RequestException as e:
+            print('Error during requests to {0} : {1}'.format(url, str(e)))
+            return None
 
 
-def is_good_response(resp):
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200
-            and content_type is not None
-            and content_type.find('html') > -1)
+    def is_good_response(self, resp):
+        content_type = resp.headers['Content-Type'].lower()
+        return (resp.status_code == 200
+                and content_type is not None
+                and content_type.find('html') > -1)
+
